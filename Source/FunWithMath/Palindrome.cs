1137b6f1a63d3812e04634c4f9f9dfc6d0e304dc
Yves Schelpe <yves.schelpe@gmail.com>
2017-08-07 13:09:56 +0200

    Added palindrome function

---
+++ Source/FunWithMath/Palindrome.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace FunWithMath
+{
+    public class Palindrome
+    {
+        public bool IsPalindrome(string candidate) {
+            if (String.IsNullOrWhiteSpace(candidate))
+                return false;
+
+            var unreversed = candidate.ToLower().Trim();
+            
+            if(unreversed[0] != unreversed[unreversed.Length-1])
+                return false;
+
+            var reversed = Reverse(unreversed);
+
+            return reversed == unreversed;            
+        }
+
+        private string Reverse(string candidate)
+        {
+            var reversed = candidate.Select((character, index) => new { index, character })
+                .OrderByDescending(x => x.index)
+                .Select(x => x.character);
+
+            return new String(reversed.ToArray());
+        }
+
+        [Theory]
+        [InlineData("Palindrome", "emordnilaP")]
+        [InlineData("Palindrome", "emordnilaP")]
+        public void CanReverseString(string candidate, string expected)
+        {
+            Assert.Equal(expected, Reverse(candidate));
+        }
+
+        [Theory]
+        [InlineData("Noon")]
+        [InlineData("Refer")]
+        [InlineData("Civic")]
+        [InlineData("Racecar")]        
+        public void ReturnsTrueWhenPalindrome(string candidate)
+        {
+            Assert.True(IsPalindrome(candidate));
+        }
+
+        [Theory]
+        [InlineData("Palindrome")]
+        public void ReturnsFalseWhenNotAPalindrome(string candidate)
+        {            
+            Assert.False(IsPalindrome(candidate));
+        }        
+    }
+}
